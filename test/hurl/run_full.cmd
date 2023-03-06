1df77d127a80041f1d1faf1493494bde1e89463e
Ilya Shinkarenko <suprematic.shinkarenko@extaccount.com>
2023-03-06 16:29:19 +0100

    Use placeholder for tableau server IP

--- test/hurl/run_full.cmd
+++ test/hurl/run_full.cmd
@@ -3,9 +3,11 @@ SET TIMEOUT_SECONDS=2
 
 :::: 7c-m1-r1
 :: SET HURL_7c_target_panel=N0MtTTEtUjE=
+:: SET HURL_tableau_server_url=<YOUR_TABLEAU_SERVER_URL>
 
-:::: shinych's old thinkstation
+:::: shinych's thinkstation1 (old)
 :: SET HURL_7c_target_panel=dGhpbmtzdGF0aW9uMQ==
+:: SET HURL_tableau_server_url=http://192.168.114.30:5000
 
 for %%i in (*.hurl) do (
     echo %%i
