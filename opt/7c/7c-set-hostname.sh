27faca000f8ac563d722da7d9dda6565a03a9319
Ilya Shinkarenko <shinych@gmail.com>
2023-05-02 18:38:06 +0200

    Start set hostname before bluetooth, write hostname to /etc/machine-info

--- opt/7c/7c-set-hostname.sh
+++ opt/7c/7c-set-hostname.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 name=`cat /sys/firmware/devicetree/base/serial-number | tail -c +9`
+
+# this is used by Python script, 7c-controller (at least)
 hostname $name
+
+# this is used by Bluetooth, s. /etc/bluetooth/main.conf
+echo 'PRETTY_HOSTNAME='$name > /etc/machine-info
+
 echo 'Set hostname to '$name | systemd-cat -p info
