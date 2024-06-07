5576f9a8c37738ab0389fc3c76a56babbd586d0f
Sergey Kozhevnikov <sergey.kozhevnikov@suprematic.net>
2024-06-07 11:30:18 +0500

    Hide set score for Americano matches

--- m1.py
+++ m1.py
@@ -401,14 +401,17 @@ class SevenCourtsM1(SampleBase):
         x_T1_score_game = X_SCORE_GAME if len(t1_game) != 1 else X_SCORE_GAME + 8
         x_T2_score_game = X_SCORE_GAME if len(t2_game) != 1 else X_SCORE_GAME + 8 
 
-        self.display_set_digit(x_set1, y_T1, FONT_SCORE, c_t1_set1, t1_set1)
-        self.display_set_digit(x_set2, y_T1, FONT_SCORE, c_t1_set2, t1_set2)
-        self.display_set_digit(x_set3, y_T1, FONT_SCORE, c_t1_set3, t1_set3)
-        graphics.DrawText(self.canvas, FONT_SCORE, x_T1_score_game, y_T1, COLOR_SCORE_GAME, t1_game)
+        # Americano
+        if match.get("isTotalPointsMatch", False) != True:
+            self.display_set_digit(x_set1, y_T1, FONT_SCORE, c_t1_set1, t1_set1)
+            self.display_set_digit(x_set2, y_T1, FONT_SCORE, c_t1_set2, t1_set2)
+            self.display_set_digit(x_set3, y_T1, FONT_SCORE, c_t1_set3, t1_set3)
 
-        self.display_set_digit(x_set1, y_T2, FONT_SCORE, c_t2_set1, t2_set1)
-        self.display_set_digit(x_set2, y_T2, FONT_SCORE, c_t2_set2, t2_set2)
-        self.display_set_digit(x_set3, y_T2, FONT_SCORE, c_t2_set3, t2_set3)
+            self.display_set_digit(x_set1, y_T2, FONT_SCORE, c_t2_set1, t2_set1)
+            self.display_set_digit(x_set2, y_T2, FONT_SCORE, c_t2_set2, t2_set2)
+            self.display_set_digit(x_set3, y_T2, FONT_SCORE, c_t2_set3, t2_set3)
+
+        graphics.DrawText(self.canvas, FONT_SCORE, x_T1_score_game, y_T1, COLOR_SCORE_GAME, t1_game)
         graphics.DrawText(self.canvas, FONT_SCORE, x_T2_score_game, y_T2, COLOR_SCORE_GAME, t2_game)
 
         # service indicator
