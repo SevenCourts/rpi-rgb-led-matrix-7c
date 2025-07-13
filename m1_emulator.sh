00080a1338c14f5f15a01574e57c3228e80c0fea
Ilya Shinkarenko <suprematic.shinkarenko@extaccount.com>
2025-07-13 20:14:09 +0200

    Use port 5005 for local emulator

--- m1_emulator.sh
+++ m1_emulator.sh
@@ -3,7 +3,7 @@
 set -eu
 
 export USE_RGB_MATRIX_EMULATOR="True"
-#export TABLEAU_SERVER_BASE_URL="http://127.0.0.1:5000"
+export TABLEAU_SERVER_BASE_URL="http://127.0.0.1:5005"
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 ./m1.sh "$@"
