73e52213507838cdace60a1f48afa72507f347d3
Ilya Shinkarenko <suprematic.shinkarenko@extaccount.com>
2025-07-16 11:27:46 +0200

    Store emulator tableau config in .runtime directory

--- m1_emulator.bat
+++ m1_emulator.bat
@@ -1,3 +1,6 @@
+mkdir .runtime
+SET PANEL_CONFIG=.runtime/panel.config
+
 SET USE_RGB_MATRIX_EMULATOR=True
 SET TABLEAU_SERVER_BASE_URL=http://127.0.0.1:5005
 :: SET TABLEAU_SERVER_BASE_URL=https://staging.server.sevencourts.com
