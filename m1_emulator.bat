3b15a483fc279e4e2c5f92747e24d546227663fe
Ilya Shinkarenko <suprematic.shinkarenko@extaccount.com>
2025-08-14 12:38:50 +0200

    Add debug switch to emulator starter

--- m1_emulator.bat
+++ m1_emulator.bat
@@ -4,4 +4,5 @@ SET PANEL_CONFIG=.runtime/panel.config
 SET USE_RGB_MATRIX_EMULATOR=True
 SET TABLEAU_SERVER_BASE_URL=http://127.0.0.1:5005
 :: SET TABLEAU_SERVER_BASE_URL=https://staging.server.sevencourts.com
+:: SET TABLEAU_DEBUG=True
 m1.bat %*
