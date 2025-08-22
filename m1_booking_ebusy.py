596be329a355e7737591f1d7c55986ed130d4c1f
Ilya Shinkarenko <suprematic.shinkarenko@extaccount.com>
2025-08-22 10:50:12 +0200

    use Matchcenter style

--- m1_booking_ebusy.py
+++ m1_booking_ebusy.py
@@ -116,7 +116,7 @@ def draw_booking(cnv, booking_info, weather_info, panel_tz):
     else:
         style = style_MatchCenter
         
-    style = style_SV1845
+    style = style_MatchCenter
 
     total_courts = len(booking_info.get('courts', []))
     if total_courts == 0:
