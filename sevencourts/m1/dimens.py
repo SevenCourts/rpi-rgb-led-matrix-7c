34900bbbd0b11207916d899d7fe3098f46dbddf4
Ilya Shinkarenko <shinych@gmail.com>
2026-05-20 15:38:35 +0200

    wip XL1

--- sevencourts/m1/dimens.py
+++ sevencourts/m1/dimens.py
@@ -2,11 +2,14 @@ import os
 
 _panel_type = os.environ.get("PANEL_TYPE", "M1")
 
-W_PANEL = 192
-
-if _panel_type == "L1":
+if _panel_type == "XL1":
+    W_PANEL = 320
+    H_PANEL = 96
+elif _panel_type == "L1":
+    W_PANEL = 192
     H_PANEL = 96
 else:  # M1
+    W_PANEL = 192
     H_PANEL = 64
 
 H_FLAG = 12
